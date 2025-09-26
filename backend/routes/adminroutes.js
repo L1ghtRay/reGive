7c55e524d662b9c897b0212582646278ebc88859
adithyan <adithyan.m.c.2004@gmail.com>
2025-09-26 23:47:00 +0530

    all other data fetching routes of admin implemented

--- backend/routes/adminroutes.js
+++ backend/routes/adminroutes.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { verifyToken } from "../middleware/verify.js";
 import { requireAdmin } from "../middleware/adminVerify.js";
-import { FetchUsers } from "../controllers/admincontroller.js";
+import {
+  FetchUsers,
+  FetchItems,
+  FetchTransactions,
+  FetchReports,
+} from "../controllers/admincontroller.js";
 const router = express.Router();
 
 router.get("/users", FetchUsers); //router.get("/users", verifyToken, requireAdmin, FetchUsers); removed authentication for testing
+router.get("/items", FetchItems);
+router.get("/transactions", FetchTransactions);
+router.get("/reports", FetchReports);
 
 export default router;
